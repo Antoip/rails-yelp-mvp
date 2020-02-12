9520c22546167a2ba4e5804f07d21df8e7d5adfe
Antoine Piguet <antoine.piguet@gmail.com>
2020-02-12 14:33:37 +0100

    Added review controller

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,14 +1,15 @@
 class RestaurantsController < ApplicationController
-  def index
+  before_action :set_restaurant, only: %i[show edit update]
 
+  def index
+    @restaurants = Restaurant.all
   end
 
   def show
-
   end
 
   def new
-
+    @restaurant = Restaurant.new
   end
 
   def create
@@ -25,5 +26,11 @@ class RestaurantsController < ApplicationController
 
   def delete
 
+  end
+
+  private
+
+  def set_restaurant
+    @restaurant = Restaurant.find(params[:id])
   end
 end
